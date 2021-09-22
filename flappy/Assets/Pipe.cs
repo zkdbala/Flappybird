75de16806710c95ef46525905d4592adb6a10e4a
Anshita Bala <anshita.bala2020@vitstudent.ac.in>
2021-09-22 23:58:24 +0530

    Add to empty project

---
+++ flappy/Assets/Pipe.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+public class Pipe : MonoBehaviour {
+
+    private Character character;
+    public GameObject pipeDown;
+
+    // Use this for initialization
+    void Start () {
+        character = FindObjectOfType<Character>(); //unity to find the character
+    }
+
+      
+    void Update () {
+        if (character.transform.position.x - transform.position.x > 30) //destroying the pipes once the dist between bird and pipe is more than 30
+        {
+
+            float xRan = Random.Range(0, 10);
+            float yRan = Random.Range(-5, 5);
+            float gapRan = Random.Range(0, 3);
+
+            Instantiate(gameObject, new Vector2(character.transform.position.x + 15 + xRan, -11 + yRan), transform.rotation); //adding another batch of pipes
+            Instantiate(pipeDown, new Vector2(character.transform.position.x + 15 + xRan, 12 + gapRan + yRan), transform.rotation);
+            Destroy(gameObject); //destroying the old ones
+
+        }
+
+    }
+
+        
+    void OnCollisionEnter2D(Collision2D other) //when it will collide with the pipe, it will die
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            character.Death();  
+        }
+    }
+}
+
---
+++ flappy/Assets/PipeD.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PipeD : MonoBehaviour {
+
+    private Character character;
+
+    // Use this for initialization
+    void Start()
+    {
+        character = FindObjectOfType<Character>(); //where the character is
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (character.transform.position.x - transform.position.x > 30) //if character is certain dist away from the downwards pipe,it will be destroyed
+        {
+          Destroy(gameObject);
+        }
+
+    }
+
+    void OnCollisionEnter2D(Collision2D other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            character.Death();
+        }
+
+    }
+
+}
